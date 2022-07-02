a66a8dce868fee5d54e1e5f54de1a00e0916dc09
Rodion Trofimov <trfimov.rodion@gmail.com>
2022-07-02 13:42:43 +0300

    set guest cookie never expire

--- src/middlewares/guest.middleware.js
+++ src/middlewares/guest.middleware.js
@@ -8,7 +8,7 @@ async function guestLogin(req,res,next) {
     }
     if (!guest) {
         guest = await guestsService.create();
-        res.cookie('guest_id',guest._id,{expires:new Date(Date.now()+900000)});
+        res.cookie('guest_id',guest._id,{expires:new Date(Date.now() + (50 * 365 * 24 * 60 * 60))});
     }
     res.locals.guest = guest;
     next();
